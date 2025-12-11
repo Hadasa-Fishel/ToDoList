99188b4d57c0d8c25c1bbc52fc932b0fd27cf3cb
הדסה פישל <dasif7616@gmail.com>
2025-12-12 00:22:54 +0200

    Fixed database connection and recreated migrations

--- ToDoApi/ToDoDbContext.cs
+++ ToDoApi/ToDoDbContext.cs
@@ -2,39 +2,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ToDoApi;
 
-public partial class ToDoDbContext : DbContext
+public class ToDoDbContext : DbContext
 {
-    public ToDoDbContext()
-    {
-    }
-
+    // בנאי (Constructor) שמקבל את אפשרויות החיבור מ-Program.cs
     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
         : base(options)
     {
     }
 
-    public virtual DbSet<Item> Items { get; set; } = null!;
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            var connectionString = "server=localhost;user=Hadasa;password=054**********;database=tododb";
-            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-        }
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Item>(entity =>
-        {
-            entity.ToTable("tasks");
-
-            entity.HasKey(e => e.Id);
-
-            entity.Property(e => e.Id).HasColumnName("Id");
-            entity.Property(e => e.Name).HasColumnName("Title");
-            entity.Property(e => e.IsComplete).HasColumnName("IsCompleted");
-        });
-    }
+    // ייצוג הטבלה במסד הנתונים
+    public DbSet<Item> Items => Set<Item>();
 }
